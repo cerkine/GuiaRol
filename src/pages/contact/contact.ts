5cca4c21f202a66d1ed09849f45879969fa8af3e
Yago Morales <yagomoralesares@gmail.com>
2019-02-13 19:55:19 +0100

    Bueno

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -11,7 +11,7 @@ export class ContactPage {
  @ViewChild(Content) content: Content;
 
  public altura:string;
- public items = ["Titulo 1", "Titulo 2", "Titulo 3"];
+ public items = ["1.Introducción","2.Descripción","3.Público objetivo","4.Cuenta","5.Funcionalidad de la app", "5.1.Ficha de personaje", "5.2.Mensaje"];
 
  constructor(public modalCtrl: ModalController) {  }
  
@@ -31,7 +31,7 @@ export class ContactPage {
   modal.onDidDismiss((data)  => {
     if (data !=null){
     let altura = document.getElementById(data.p1).offsetTop;
-    this.content.scrollTo(0, altura, 700);
+    this.content.scrollTo(0, altura-15, 700);
     }
   });
 
